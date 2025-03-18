f05aa8065df9cb161a72a26084e55198e690f847
lizyweb <147726740+lizyweb@users.noreply.github.com>
2025-03-18 11:02:40 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,47 @@
+document.addEventListener('DOMContentLoaded', () => {
+  "use strict";
+
+  window.onpageshow = function(event) {
+    if (event.persisted) {
+      location.reload(); // Reload the page when navigated back to
+    }
+  };
+
+  jQuery(function ($) {
+    'use strict';
+  
+    $(document).ready(function () {
+  
+       // Hero Header carousel
+  $(".header-carousel").owlCarousel({
+    animateOut: 'slideOutDown',
+    items: 1,
+    autoplay: true,
+    smartSpeed: 1000,
+    dots: false,
+    loop: true,
+    nav : true,
+    navText : [
+        '<i class="bi bi-arrow-left"></i>',
+        '<i class="bi bi-arrow-right"></i>'
+    ],
+});
+
+  
+    });
+  
+  });
+
+  
+  // Testimonials carousel
+$(".testimonial-carousel").owlCarousel({
+  autoplay: true,
+  smartSpeed: 1000,
+  items: 1,
+  dots: true,
+  loop: true,
+});
+
+
+
+});
